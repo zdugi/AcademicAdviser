25b93195be5b416d57f9605406f4f1adadfb5f86
roki021 <26367195+roki021@users.noreply.github.com>
2021-05-10 16:46:03 +0200

    Setup of spring

---
+++ server/src/main/java/com/academic/adviser/AdviserApplication.java
@@ -0,0 +1,13 @@
+package com.academic.adviser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdviserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdviserApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/academic/adviser/AdviserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academic.adviser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdviserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
